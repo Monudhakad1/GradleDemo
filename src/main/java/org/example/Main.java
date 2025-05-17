4713b1ff1cd5fd3298785d54855566422a8582d1
Monudhakad1 <monudhakad055@gmail.com>
2025-05-18 00:42:24 +0530

    added okhttp with plain java code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,29 @@
 package org.example;
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello World!");
+        OkHttpClient client = new OkHttpClient();
+        String url = "https://example.com/";
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) {
+                System.out.println("Something went wrong");
+            } else {
+                System.out.println(response.body().string());  // ✅ correct usage
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
